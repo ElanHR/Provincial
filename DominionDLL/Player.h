effc98f16afdd24f95b93db1645b65a11ca4cb9d
isep <mail@elanhr.org>
2013-12-09 21:10:09 -0500

    Added PlayerStateInformed class (currently just a copy of heuristic player)

--- DominionDLL/Player.h
+++ DominionDLL/Player.h
@@ -58,6 +58,42 @@ private:
     int _remodelGoldThreshold;
 };
 
+class PlayerStateInformed : public Player
+{
+public:
+    PlayerStateInformed(const BuyAgenda *agenda);
+    void MakeDecision(const State &s, DecisionResponse &response);
+    String ControllerName() { return "Heuristic " + _agenda->Name(); }
+    const BuyAgenda& Agenda() const
+    {
+        return *_agenda;
+    }
+
+    PlayerStateInformed* Mutate(const CardDatabase &cards, const GameOptions &options) const;
+
+private:
+    bool CardDesired(const State &s, int player, Card *c) const;
+    void MakePhaseDecision(const State &s, DecisionResponse &response);
+    void MakeBaseDecision(const State &s, DecisionResponse &response);
+    void MakeIntrigueDecision(const State &s, DecisionResponse &response);
+    void MakeSeasideDecision(const State &s, DecisionResponse &response);
+    void MakeAlchemyDecision(const State &s, DecisionResponse &response);
+    void MakeProsperityDecision(const State &s, DecisionResponse &response);
+    void MakeCustomDecision(const State &s, DecisionResponse &response);
+
+    void MakeCopyDecision(const State &s, DecisionResponse &response);
+    void MakeDiscardDownDecision(const State &s, DecisionResponse &response);
+    void MakeDeckReorderDecision(const State &s, DecisionResponse &response);
+    void MakeTrashDecision(const State &s, DecisionResponse &response);
+    
+    Vector<Card*> TrashableCards(const State &s) const;
+    UINT TrashableCardCount(const State &s) const;
+    UINT DiscardableCardCount(const State &s) const;
+
+    const BuyAgenda *_agenda;
+    int _remodelGoldThreshold;
+};
+
 class PlayerSmart : public Player
 {
 public:
