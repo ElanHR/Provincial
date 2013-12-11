411ed3692902ab8e7929524438b758889428220c
Elan <elan.hourticolon.retzler@gmail.com>
2013-12-11 17:26:10 -0500

    rework- starting to develop features

--- DominionDLL/DecisionStrategy.cpp
+++ DominionDLL/DecisionStrategy.cpp
@@ -25,10 +25,18 @@ double DecisionStrategy::getDecisionWeight(const State &s, Decisions d){
 
 		double curW;
 
+		//  MONEY_DENSITY_OF_DECK,
+		//  MONEY_DENSITY_OF_DECK_GREATER_THAN_1,
+		//  MONEY_DENSITY_OF_DECK_GREATER_THAN_1_5,
+		//  MONEY_DENSITY_OF_DECK_GREATER_THAN_2,
+		//  ACTION_DENSITY_OF_DECK,
+		//  VICTORY_DENSITY_OF_DECK,
+
 		switch (f.type){
 			case OPPONENT_HAS_ATTACK_CARDS:
 				// get value of OPPONENT_HAS_ATTACK_CARDS from state s and multiphy in 
 				//TODO: ACTUALLY EXTRACT INFO FROM S
+				s.data->players[0].
 				curW = f.weight * 1.0;
 				break;
 
