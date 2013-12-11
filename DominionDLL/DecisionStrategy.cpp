65a1f98bda3492e5e26758ad432078fdba0246fd
Elan <elan.hourticolon.retzler@gmail.com>
2013-12-11 01:37:57 -0500

    created DecisionStrategy class

---
+++ DominionDLL/DecisionStrategy.cpp
@@ -0,0 +1,13 @@
+#include "DecisionStrategy.h"
+
+DecisionStrategy::DecisionStrategy(){
+
+}
+
+DecisionStrategy::DecisionStrategy(DecisionStrategy &m){
+
+}
+
+DecisionStrategy* DecisionStrategy::Mutate() const{
+	return 0;
+}
